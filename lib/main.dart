652fb0eb646f8e3b5a41c561bad633253b6e81e9
harrison <harrisonpinchieri@gmail.com>
2022-08-14 14:54:07 -0300

    contador_v2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:contador_de_dinheiro/screens/Inital_screen.dart';
+
 import 'package:contador_de_dinheiro/widget/NavBar.dart';
 import 'package:flutter/material.dart';
 
@@ -17,10 +17,68 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(),
+      home: Scaffold(
+        appBar: AppBar (
+
+        ),
+        drawer: NavBar(),
+        body: ListView(
+          children: [
+
+            money('teste',4),
+
+            
+            
+          ],
+        ),
+
+      ),
     );
 
   }
 }
 
+class money extends StatefulWidget {
+
+  final String foto;
+  final int valorTotal;
+
+  const money(this.foto,  this.valorTotal,{Key? key}) : super(key: key);
+
+  @override
+  State<money> createState() => _moneyState();
+}
+
+class _moneyState extends State<money> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        children: [
+          Container(
+            child: Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4),
+                    color: Colors.black26,
+
+                  ),
+                  width:1000,
+                  height: 100,
+
+                )
+              ],
+            ),
+
+          ),
+        ],
+
+
+      ),
+    );
+  }
+}
 
